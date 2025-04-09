8c99931ba30eeff729018caf27a0018bc9e0bf63
Tejada Rosales <miguelrt@boston.cs.colostate.edu>
2025-04-09 09:02:33 -0600

    eh

--- group-app/src/app/app.component.ts
+++ group-app/src/app/app.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavComponent],
-  templateUrl: './app.component.html',
+  template: `
+    <app-nav></app-nav>
+    <router-outlet></router-outlet>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {

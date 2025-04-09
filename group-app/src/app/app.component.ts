47374bc1d4c42f2fb2f3ab22b43dca4b4c66b76a
Tejada Rosales <miguelrt@boston.cs.colostate.edu>
2025-04-09 09:54:54 -0600

    change

--- group-app/src/app/app.component.ts
+++ group-app/src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   template: `
-    <app-nav></app-nav>
-
-    <h2>Welcome to {{ title }}!</h2>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, qui nostrum odio vel alias, at ex eius voluptatum eligendi fugit perferendis consequuntur voluptate quibusdam doloremque, reprehenderit iste! Doloremque, sapiente quam!</p>
-
-    <router-outlet></router-outlet>
+    <app-nav />
+    <app-home />
   `,
   styleUrl: './app.component.css'
 })

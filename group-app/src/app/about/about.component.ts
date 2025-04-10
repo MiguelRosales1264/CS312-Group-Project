6bb79f4256d18f60e846eef7f99fcff3a5bb687f
Tejada Rosales <miguelrt@boston.cs.colostate.edu>
2025-04-10 07:35:31 -0600

    Removed provider

--- group-app/src/app/about/about.component.ts
+++ group-app/src/app/about/about.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { AboutService } from '../services/about.service';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css',
-  providers: [ AboutService ],
+  styleUrl: './about.component.css'
 })
 export class AboutComponent {
 

7e2db03ea51905fc992add39a8bdf1984cecfd8b
Tejada Rosales <miguelrt@boston.cs.colostate.edu>
2025-04-10 07:34:35 -0600

    Title var

--- group-app/src/app/about/about.component.ts
+++ group-app/src/app/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { AboutService } from '../services/about.service';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
+  providers: [ AboutService ],
 })
 export class AboutComponent {
 

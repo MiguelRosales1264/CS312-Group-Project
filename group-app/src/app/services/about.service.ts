a0b17738bc6dd87a055ac18f3668dbe74f1ac939
Tejada Rosales <miguelrt@boston.cs.colostate.edu>
2025-04-10 07:40:31 -0600

    Person model

--- group-app/src/app/services/about.service.ts
+++ group-app/src/app/services/about.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Person } from '../model/person.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AboutService {
+
+  aboutPersons: Array<Person> = [];
 
   constructor() { }
 }

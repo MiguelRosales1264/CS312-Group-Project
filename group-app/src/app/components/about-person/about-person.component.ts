1d13b8c8ee8d4bf88aa21b1b05a09d4c0b966888
Tejada Rosales <miguelrt@boston.cs.colostate.edu>
2025-04-10 07:31:22 -0600

    Person Component & Services

---
+++ group-app/src/app/components/about-person/about-person.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-person',
+  imports: [],
+  templateUrl: './about-person.component.html',
+  styleUrl: './about-person.component.css'
+})
+export class AboutPersonComponent {
+
+}

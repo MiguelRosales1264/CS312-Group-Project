cb54b2dfd48e34ef70480095ca6fb342850520e7
Jonathan Evans <u1098099@colostate.edu>
2025-04-10 22:23:40 -0600

    Removed the changes to main. They were unnecessary

--- group-app/src/main.ts
+++ group-app/src/main.ts
@@ -1,10 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { ColorGenerationComponent } from './app/color-generation/color-generation.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
-  bootstrapApplication(ColorGenerationComponent)
-  .catch(err => console.error(err));

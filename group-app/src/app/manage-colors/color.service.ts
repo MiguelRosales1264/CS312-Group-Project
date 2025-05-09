73554ed8b645ae7a428bb6fd44bdc3e8723ecc1e
Tejada Rosales <miguelrt@boston.cs.colostate.edu>
2025-05-08 18:26:25 -0600

    correct path

--- group-app/src/app/manage-colors/color.service.ts
+++ group-app/src/app/manage-colors/color.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Color } from 'src/app/manage-colors/color.model';
+import { Color } from './color.model';
 
 @Injectable({
     providedIn: 'root'
